69453dd8e93da7852e89dfc36d54bd4d2845b57f
Mohit Uniyal <mohituniyal46@gmail.com>
2023-09-04 11:34:24 +0530

    modified app.js

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use('/', router);
 const dirname=path.resolve()
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(dirname, 'client', 'build')));
-    app.get('*', (req, res)=>{
-        res.sendFile(path.join(dirname, 'client', 'build', 'index.html'));
-    })
+    // app.get('*', (req, res)=>{
+    //     res.sendFile(path.join(dirname, 'client', 'build', 'index.html'));
+    // })
 }else{
     app.use('/', router);
 }
